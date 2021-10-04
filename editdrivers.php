88ac3ff123714ab5efce7dbb8f35378b782ab0e0
EkoateDev <ekoate@globaledge-software.com>
2021-10-04 17:23:45 +0400

    added edit drivers page need to make it work

--- editdrivers.php
+++ editdrivers.php
@@ -9,17 +9,17 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 
-    <title>Profile Card!</title>
+    <title>Edit Card!</title>
 </head>
 
 <body>
     <div class="container-lg">
-        <h1>Displaying different Profiles for F1 Drivers</h1>
+        <h1>Edit Drivers here </h1>
         <br /><br />
         <!-- Button trigger modal -->
         <div class="">
             <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Create a Driver
+                Edit a Driver
             </button>
         </div>
         <br /><br />
@@ -34,7 +34,7 @@
                         </h5>
                     </div>
                     <div class="modal-body">
-                        <form class="align-items-center" action="upload.php" method="POST" onSubmit="uploadSubmit();" enctype="multipart/form-data">
+                        <form class="align-items-center" action="" method="POST" onSubmit="uploadSubmit();" enctype="multipart/form-data">
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" name="title" id="title" placeholder="Enter your Name" value="" required />
                                 <label for="title" class="form-label">Name</label>
@@ -72,41 +72,6 @@
                 </div>
             </div>
         </div>
-
-        <?php
-
-        include "connection.php"; // Using database connection file here
-
-        $select = mysqli_query($conn, "SELECT * From drivers"); // fetch data from database
-
-        while ($record = mysqli_fetch_array($select)) {
-
-        ?>
-            <div class="row row-cols-1 row-cols-md-3 g-4 text-center">
-                <div class="col">
-                    <div class="card h-100">
-                        <div class="avatar">
-                            <img src="uploads/<?php echo $record['image']; ?>" class="card-img-top" alt="" />
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title text-center text-uppercase text-muted" id="title"><?php echo $record['title']; ?>
-                            </h5>
-                            <div class="text-center fs-4" id="description">Brief Details:<br> <?php echo $record['description']; ?></div>
-                            <div class="text-center fs-5 text-white bg-success" id="champ">Championship:<br> <?php echo $record['champ']; ?></div>
-                            <div class="text-center fs-5 text-white bg-dark" id="team">Team: <?php echo $record['team']; ?></div><br>
-                            <a href="editdrivers.php" class="btn btn-outline-success" >Edit Driver</a>
-                            <a href="#" class="btn btn-outline-danger">Delete Driver</a>
-                        </div>
-                    </div>
-                </div>
-            </div><br>
-
-        <?php
-        }
-        ?>
-
-        <?php $conn->close(); // Close connection 
-        ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="verification.js"></script>
